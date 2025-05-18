d8d471073a2fff7571a2c67c27f8969ad8ab06dd
MelihEmreGuler <gulermelihemre@gmail.com>
2025-05-19 00:27:05 +0300

    add categories to init sql

--- init.sql
+++ init.sql
@@ -30,6 +30,17 @@ CREATE TABLE IF NOT EXISTS category (
     unit_type VARCHAR NOT NULL
 );
 
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM category WHERE category_id = 1) THEN
+        INSERT INTO category (category_id, waste_type, unit_type) VALUES
+            (1, 'Plastic', 'kg'),
+            (2, 'Paper', 'kg'),
+            (3, 'Glass', 'piece');
+    END IF;
+END
+$$;
+
 CREATE TABLE IF NOT EXISTS appointment (
     appointment_id INT PRIMARY KEY,
     note VARCHAR,
